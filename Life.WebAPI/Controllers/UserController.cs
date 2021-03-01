51b062168be488a0f3e40901ba20bd4900c19b5e
Jonatas Almeida <jonatas-almeida@github.com>
2021-03-01 03:03:15 -0300

    Criação da Controller de Login

---
+++ Life.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,149 @@
+﻿using AutoMapper;
+using Life.Domain.Identity;
+using Life.WebAPI.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        private readonly UserManager<User> _userManager;
+
+        private readonly SignInManager<User> _signInManager;
+
+        private readonly IMapper _mapper;
+
+
+        public UserController(IConfiguration config, UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
+        {
+            _config = config;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+
+
+        //Pega todos os usuários
+        [HttpGet("GetUser")]
+        public async Task<IActionResult> GetUser()
+        {
+            return Ok(new UserDto());
+        }
+
+
+        //Método register
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            try
+            {
+                var user = _mapper.Map<User>(userDto);
+                var result = await _userManager.CreateAsync(user, userDto.Password);
+                var userToReturn = _mapper.Map<UserDto>(user);
+
+                if (result.Succeeded)
+                {
+                    return Created("GetUser", userToReturn);
+                }
+
+                return BadRequest(result.Errors);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados falhou");
+            }
+        }
+
+
+        //Método de Login
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(UserLoginDto userLogin)
+        {
+            try
+            {
+                var user = await _userManager.FindByNameAsync(userLogin.UserName);
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, userLogin.Password, false);
+
+                if (result.Succeeded)
+                {
+                    var appUser = await _userManager.Users.FirstOrDefaultAsync(u => u.NormalizedUserName == userLogin.UserName.ToUpper());
+
+                    var userToReturn = _mapper.Map<UserLoginDto>(appUser);
+
+                    return Ok(new
+                    {
+                        token = GenerateJWToken(appUser).Result,
+                        user = userToReturn
+                    });
+                }
+
+                return Unauthorized();
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao tentar fazer o login!");
+            }
+        }
+
+
+        //Método de gerar o token
+        private async Task<string> GenerateJWToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            foreach(var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            //Pega a chave definida no JSON
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            //Criptografa a chave
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            //Valida a chave
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            //Cria o token a partir da validação
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            //Retorna o token
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
