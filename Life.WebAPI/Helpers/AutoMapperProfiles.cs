a067a51936703fda81d16da581ef49afb7f54184
Jonatas Almeida <jonatas-almeida@github.com>
2021-03-01 03:22:01 -0300

    Criação do AutoMapperProfiles

---
+++ Life.WebAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Life.Domain;
+using Life.WebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Life.WebAPI.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Consulta, ConsultaDto>().ForMember(dest => dest.DoctorConsultas, opt => {
+                opt.MapFrom(src => src.DoctorsConsultas.Select(x => x.Consulta).ToList());
+               }).ReverseMap();
+
+            //CONTINUAR DAQUI
+            //Analisar a classe de Dtos de Médicos e Consultas
+        }
+    }
+}
